f1ca3d91a97deeba4001d52cb6b2aa56a7de0e2e
Tim Tegeler <tim@tegeler.me>
2016-04-13 11:24:13 +0200

    First tests

--- phpunit/tests/ResponseBuilderTest.php
+++ phpunit/tests/ResponseBuilderTest.php
@@ -1,14 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tim
- * Date: 10.04.16
- * Time: 14:24
- */
 
-namespace Creios;
+namespace Creios\Creiwork\Framework;
+
+use Creios\Creiwork\Framework\Result\TemplateResult;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
+use GuzzleHttp\Psr7\Stream;
+use GuzzleHttp\Psr7\Uri;
+use League\Plates\Engine;
 
 class ResponseBuilderTest extends \PHPUnit_Framework_TestCase
 {
 
+//    /** @var ResponseBuilder */
+//    private $responseBuilder;
+//    /** @var \PHPUnit_Framework_MockObject_MockObject|Engine */
+//    private $engine;
+//    /** @var \PHPUnit_Framework_MockObject_MockObject|Response */
+//    private $response;
+//
+//    public function setUp()
+//    {
+//        $this->engine = $this->getMock(Engine::class);
+//        $this->responseBuilder = new ResponseBuilder($this->engine);
+//        $this->response = new Response();
+//    }
+//
+//    public function testTemplateResult()
+//    {
+//
+//        $this->response->withHeader('Content-Type','text/html')->withBody(new Stream(''));
+//        $result = new TemplateResult('test', []);
+//        $this->assertEquals(), $this->responseBuilder->process($result));
+//    }
 }
