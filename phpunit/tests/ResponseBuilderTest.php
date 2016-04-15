947109ebfb0cd6d8903998bb21653ba4d464f958
Tim Tegeler <tim@tegeler.me>
2016-04-15 09:21:49 +0200

    First test

--- phpunit/tests/ResponseBuilderTest.php
+++ phpunit/tests/ResponseBuilderTest.php
@@ -4,33 +4,37 @@ namespace Creios\Creiwork\Framework;
 
 use Creios\Creiwork\Framework\Result\TemplateResult;
 use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\ServerRequest;
 use GuzzleHttp\Psr7\Stream;
-use GuzzleHttp\Psr7\Uri;
 use League\Plates\Engine;
+use Zumba\Util\JsonSerializer;
 
 class ResponseBuilderTest extends \PHPUnit_Framework_TestCase
 {
 
-//    /** @var ResponseBuilder */
-//    private $responseBuilder;
-//    /** @var \PHPUnit_Framework_MockObject_MockObject|Engine */
-//    private $engine;
-//    /** @var \PHPUnit_Framework_MockObject_MockObject|Response */
-//    private $response;
-//
-//    public function setUp()
-//    {
-//        $this->engine = $this->getMock(Engine::class);
-//        $this->responseBuilder = new ResponseBuilder($this->engine);
-//        $this->response = new Response();
-//    }
-//
-//    public function testTemplateResult()
-//    {
-//
-//        $this->response->withHeader('Content-Type','text/html')->withBody(new Stream(''));
-//        $result = new TemplateResult('test', []);
-//        $this->assertEquals(), $this->responseBuilder->process($result));
-//    }
+    /** @var ResponseBuilder */
+    private $responseBuilder;
+    /** @var \PHPUnit_Framework_MockObject_MockObject|Engine */
+    private $engine;
+    /** @var \PHPUnit_Framework_MockObject_MockObject|JsonSerializer */
+    private $serializer;
+
+    public function setUp()
+    {
+        $this->engine = $this->getMock(Engine::class);
+        $this->serializer = $this->getMock(JsonSerializer::class);
+        $this->responseBuilder = new ResponseBuilder($this->serializer, $this->engine);
+    }
+
+    public function testTemplateResult()
+    {
+        $stream = fopen('php://temp', 'r+');
+        fwrite($stream, '');
+        fseek($stream, 0);
+        $assertedResponse = (new Response())->withHeader('Content-Type', 'text/html')->withBody(new Stream($stream));
+
+        $result = new TemplateResult('test', []);
+        $actualResponse = $this->responseBuilder->process($result);
+
+        $this->assertEquals($assertedResponse->getHeaders(), $actualResponse->getHeaders());
+    }
 }
