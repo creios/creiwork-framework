01d8435b56469e8205b8cf982d0fae7c8b471083
Tim Tegeler <tim@tegeler.me>
2016-06-13 20:58:50 +0200

    Avoiding code issue in phpstorm

--- phpunit/tests/ResponseBuilderTest.php
+++ phpunit/tests/ResponseBuilderTest.php
@@ -54,7 +54,7 @@ class ResponseBuilderTest extends \PHPUnit_Framework_TestCase
 
     public function testTemplateResultWithStatusCode()
     {
-        $assertedResponse = (new Response())->withHeader('Content-Type', 'text/html')->withHeader('Content-Length', 0)->withStatus(200)->withBody(new Stream($this->stream));
+        $assertedResponse = (new Response())->withStatus(200)->withHeader('Content-Type', 'text/html')->withHeader('Content-Length', 0)->withBody(new Stream($this->stream));
         $result = (new TemplateResult('test', []))->withStatusCode(200);
         $actualResponse = $this->responseBuilder->process($result);
         $this->assertEquals($assertedResponse->getHeaders(), $actualResponse->getHeaders());
