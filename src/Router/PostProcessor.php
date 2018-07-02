d6820c233b361efbd610136c6a863c49451180c1
Benjamin Knauer <knauer@creios.net>
2018-07-02 14:51:19 +0200

    renamed host varaible and corrected logic, closes #5

--- src/Router/PostProcessor.php
+++ src/Router/PostProcessor.php
@@ -144,7 +144,7 @@ class PostProcessor implements PostProcessorInterface
         if ($this->sharedDataProvider->hasData()) {
             $this->templateEngine->addData($this->sharedDataProvider->getData());
         }
-        $this->templateEngine->addData(['host' => 'http://' . $this->serverRequest->getServerParams()['HTTP_HOST'] . '/']);
+        $this->templateEngine->addData(['hostWithProtocol' => ($this->serverRequest->getServerParams()['HTTPS'] ? 'https://' : 'http://') . $this->serverRequest->getServerParams()['HTTP_HOST'] . '/']);
         if ($templateResult->getData() === null) {
             $data = [];
         } else {
